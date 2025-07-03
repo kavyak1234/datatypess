e9c774f6fea9a86817964fe85dc75f245d165904
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-03 17:41:18 +0530

    question5 is completed,3/7/2025

---
+++ data/question5.js
@@ -0,0 +1,6 @@
+function sumNumbers(a, b) {
+  return Number(a) + Number(b);
+}
+console.log(sumNumbers("10", 5));    // Output: 15
+console.log(sumNumbers("7", "3"));   // Output: 10
+console.log(sumNumbers(4, 6));       // Output: 10
